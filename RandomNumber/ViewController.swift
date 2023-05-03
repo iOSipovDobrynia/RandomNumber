bcdfd71f63b97c0d6b719d91fe433b9c4c148746
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-03 21:32:19 +0300

    add text and random generator

--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: IBOutlets
+    @IBOutlet var minimumNumberLabel: UILabel!
+    @IBOutlet var maximumNumberLabel: UILabel!
+    @IBOutlet var generatedNumberLabel: UILabel!
+    
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: IBActions
+    @IBAction func getButtonPressed() {
+        let min = Int(minimumNumberLabel.text ?? "") ?? 0
+        let max = Int(maximumNumberLabel.text ?? "") ?? 100
+        
+        generatedNumberLabel.text = Int.random(in: min...max).formatted()
+    }
 
 }
 
