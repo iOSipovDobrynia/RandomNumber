8a4800594314960cf911c5dfdc24ff58b9cdc17b
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-04 08:31:33 +0300

    add settingsVC

---
+++ RandomNumber/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  RandomNumber
+//
+//  Created by Goodwasp on 04.05.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
