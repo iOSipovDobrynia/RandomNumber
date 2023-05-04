ddfb99bcfccaa0fb334d45e7a97981d97e027032
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-04 09:04:41 +0300

    add logic

--- RandomNumber/SettingsViewController.swift
+++ RandomNumber/SettingsViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
 
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
+    }
+    
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
     }
 }
--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -19,12 +19,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {return }
+        settingsVC.maximumValue = maximumNumberLabel.text
+        settingsVC.minimumValue = minimumNumberLabel.text
+    }
+    
     // MARK: IBActions
     @IBAction func getButtonPressed() {
         let min = Int(minimumNumberLabel.text ?? "") ?? 0
         let max = Int(maximumNumberLabel.text ?? "") ?? 100
         
         generatedNumberLabel.text = Int.random(in: min...max).formatted()
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVS = segue.source as? SettingsViewController else {return }
+        minimumNumberLabel.text = settingsVS.minimumValueTF.text
+        maximumNumberLabel.text = settingsVS.maximumValueTF.text
     }
 
 }
